92aac3d92fbb34006330a51262af3a0230e68284
Deiverson-Almeida <deiverson_almeida@globomail.com>
2021-11-20 01:25:37 -0300

    new steak Article document

--- script1.R
+++ script1.R
@@ -15,3 +15,25 @@ ggplot(sdat,
        aes(x = steak_prep, fill = female)) + 
   geom_bar(position="dodge") +
   ggtitle(paste0("Steak Preparation Preference by Gender for ","Mountain"," Region"))
+
+
+
+
+#format(Sys.Date(), '%A, %d de %B de %Y')
+#str_to_title(format(Sys.Date(), '%A, %d de %B de %Y'))
+
+
+
+
+
+####################################################################################################
+#paste0(str_to_title(format(Sys.Date(), '%A, %d ')), "de ", 
+#       str_to_title(format(Sys.Date(), '%B ')), "de ",
+#       str_to_title(format(Sys.Date(), '%Y')))
+
+
+#suppressMessages(suppressWarnings(library(tidyverse)))
+
+
+
+
